a9f36461bcffb7cb822ca686d67bfef6660b77d8
陈开哥 <18278790880@163.com>
2026-05-25 21:56:04 +0800

    今天完成了 Order,Trade 等数据结构

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod model;
 
-use model::{Order, Side};
+use model::{Order, Side, Trade};
 
 fn main() {
     let order = Order {
@@ -11,5 +11,16 @@ fn main() {
         quantity: 1,
     };
 
-    println!("{}", order.symbol);
+    println!("new order: {}", order.symbol);
+
+    let trade = Trade {
+        trade_id: 1,
+        order_id: order.id,
+        symbol: order.symbol.clone(),
+        price: 100_000,
+        quantity: 1,
+        timestamp: 1_717_000_000,
+    };
+
+    println!("trade executed: {:?}", trade);
 }
--- src/model.rs
+++ src/model.rs
@@ -17,3 +17,13 @@ pub struct Order {
     pub price: i64,
     pub quantity: u64,
 }
+
+#[derive(Debug)] 
+pub struct Trade {
+    pub trade_id: u64,
+    pub order_id: u64,
+    pub symbol: String,
+    pub price: i64,
+    pub quantity: u64,
+    pub timestamp: u64,
+}
